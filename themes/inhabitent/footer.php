24b68ba60ae90aec948b30e8356fe792c11f32be
Cliff Chan <cchan17@live.ca>
2018-02-19 15:35:49 -0800

    initial commit

---
+++ themes/inhabitent/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+				</div><!-- .site-info -->
+
+				<div>
+					<h3>Contact Info</h3>
+					<div><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></div>
+					<div><a href="tel:778-456-7891">778-567-7891</a></div>
+
+				<div>
+					<h3>Business Hours</h3>
+					<p><span>Monday-Friday:</span> 9am to 5pm</p>
+
+					<p><span>Saturday:</span> 10am to 2pm</p>
+
+					<p><span>Sunday:</span> Closed</p>
+				</div>
+
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ themes/inhabitent/inc/extras.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package RED_Starter_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function inhabitent_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'red_starter_body_classes' );
+
+function inhabitent_logo() { ?>
+	<style type="text/css">
+			#login h1 a, .login h1 a {
+					background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-text-dark.svg);
+	height:65px;
+	width:320px;
+	background-size: 320px 65px;
+	background-repeat: no-repeat;
+				padding-bottom: 30px;
+			}
+	</style>
+<?php }
+add_action( 'login_enqueue_scripts', 'inhabitent_logo' );
+
+add_filter( 'login_headerurl', 'custom_loginlogo_url' );
+function custom_loginlogo_url($url) {
+   return get_home_url();
+}
+
+/**
+* Customize the title attritbute for the login logo link.
+* @return string
+*/
+
+function inhabitent_login_title(){
+	return 'Inhabitent Camping Supply Co.';
+}
+
+add_filter('login_headertitle', 'inhabitent_login_title');
+
+?>
