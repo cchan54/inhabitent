9ef3554c7ba9bc78b21e0d5969e3dc0e67e071e6
Cliff Chan <cchan17@live.ca>
2018-02-20 13:29:21 -0800

    added plugin

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -4,11 +4,16 @@
  *
  * @package RED_Starter_Theme
  */
-
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<section class="front-page-hero">
+				<img src"<?php echo get_site_url(); ?>/images/logos/inhabitent-logo-full.svg/">
+			</section>
+			
+		<div class="product-shop container">
+        <h2>Shop Stuff</h2>
+		</div>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
