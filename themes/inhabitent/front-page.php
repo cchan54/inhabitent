633f5ddab91638ccda4527d7be2a2ece432ad843
Cliff Chan <cchan17@live.ca>
2018-02-22 15:25:10 -0800

    added fonts and styling to Shop Stuff

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -40,45 +40,42 @@
 
 			<section class="latest-entries">
 					<div class="container">
-						
 						</div><!-- .entry-meta -->
+
 				<?php while ( have_posts() ) : the_post(); ?>
-
 					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+						<?php endwhile; // End of the loop. ?>
+							</main><!-- #main -->
+								</div><!-- #primary -->
 
-				<?php endwhile; // End of the loop. ?>
-
-			</main><!-- #main -->
-
-		</div><!-- #primary -->
 		<section class="latest-entries">
 			<h2>Inhabitent Journal</h2>
-			<div class="most-recent-journals">
-				<?php
-					$args = array( 'post_type' => 'post', 'order' => 'DESC', 'posts_per_page' => 3, 'orderby' => 'date' );
-						$journal_posts = get_posts( $args ); // returns an array of posts
-				?>
+				<div class="most-recent-journals">
+					<?php
+						$args = array( 'post_type' => 'post', 'order' => 'DESC', 'posts_per_page' => 3, 'orderby' => 'date' );
+							$journal_posts = get_posts( $args ); // returns an array of posts
+								?>
+
 			<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
 			<div class="journal-recent-block-item">
-					<div class="journal-thumbnail-wrapper">
-							<?php if ( has_post_thumbnail() ) : ?>
-									<?php the_post_thumbnail( 'large' ); ?></a>
-										<?php endif; ?>     
-											</div>
-										<div class="entry-wrapper">
-									<div class="entry-meta">
-								<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_on(); ?>
-							</div><!-- .entry-meta -->	
+				<div class="journal-thumbnail-wrapper">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'large' ); ?></a>
+							<?php endif; ?>     
+								</div>
+									<div class="entry-wrapper">
+										<div class="entry-meta">
+											<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_on(); ?>
+												</div><!-- .entry-meta -->	
 
-						<h3><a class="journal-title" href="<?php echo get_post_permalink() ?>"><?php the_title(); ?></a></h3>
-																				
+						<h3><a class="journal-title" href="<?php echo get_post_permalink() ?>"><?php the_title(); ?></a></h3>												
 							</div><!-- .entry-wrapper -->
 								<div class="entry-readmore link-button">
 									<a href="<?php echo get_post_permalink()?>">Read Entry</a>
-						</div><!-- .entry-readmore -->
-        </div><!-- .journal-recent-block-item -->
+										</div><!-- .entry-readmore -->
+        							</div><!-- .journal-recent-block-item -->
 
-		<?php endforeach; wp_reset_postdata(); ?>
+			<?php endforeach; wp_reset_postdata(); ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
