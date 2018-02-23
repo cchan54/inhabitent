1f8543e7679af5d144e29e028a2432ef146f6db1
Cliff Chan <cchan17@live.ca>
2018-02-23 13:23:05 -0800

    finished front page and about page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -4,78 +4,46 @@
  *
  * @package RED_Starter_Theme
  */
-	get_header(); ?>
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
-				<section class="front-page-hero">
-					<img src"<?php echo get_site_url(); ?>/images/logos/inhabitent-logo-full.svg/">
-				</section>
+get_header(); ?>
 
-				<!--Product Terms -->
-				<section class="product-info container">
-						<h2>Shop Stuff</h2>	
-					<?php
-				$terms = get_terms( array(
-						'taxonomy'   => 'product_type',
-						'hide_empty' => 0,
-				) );
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-				if ( ! empty( $terms ) ) :
-						?>
-						<div class="product-type-blocks">
-								<?php foreach ( $terms as $term ) :?>
-										<div class="product-type-block-wrapper">
-												<img src="<?php echo get_template_directory_uri() . '/images/' . $term->slug; ?>.svg"
-														 alt="<?php echo $term->name; ?>"/>
-												<p><?php echo $term->description; ?></p>
-												<p>
-														<a href="<?php echo get_term_link( $term ); ?>"
-															class="btn"><?php echo $term->name; ?> Stuff</a>
-												</p>
-										</div>
-								<?php endforeach; ?>
-						</div>
-				<?php endif; ?>
-		</section>
+	<?php if ( have_posts() ) : ?>
 
-			<section class="latest-entries">
-					<div class="container">
-						</div><!-- .entry-meta -->
+		<header class="page-header">
+			<?php the_archive_title('<h1 class="page-title">', '</h1>')//the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+			<?php 
+				$args = array( 'post_type' => 'product-type' );
+				$terms = get_terms( $args );
+			?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'template-parts/content', 'page' ); ?>
-						<?php endwhile; // End of the loop. ?>
-							</main><!-- #main -->
-								</div><!-- #primary -->
+			<div class="product-stuff-links">
+				<?php foreach ($terms as $term): ; ?>
+				<a href="<?php echo get_site_url(); ?>/<?php echo $term->taxonomy; ?>/<?php echo $term->slug; ?>"><?php echo $term->name; ?></a>
+				<?php endforeach; ?>
+			</div>
+		</header><!-- .page-header -->
 
-		<section class="latest-entries">
-			<h2>Inhabitent Journal</h2>
-				<div class="most-recent-journals">
-					<?php
-						$args = array( 'post_type' => 'post', 'order' => 'DESC', 'posts_per_page' => 3, 'orderby' => 'date' );
-							$journal_posts = get_posts( $args ); // returns an array of posts
-								?>
+					<?php /* Start the Loop */ ?>
+					
+					<div class="product-archive-grid">
+							<?php while ( have_posts() ) : the_post(); ?>
+									<div class="product-grid-item">
+											<?php
+													get_template_part( 'template-parts/product', 'archive' );
+											?>
+									</div>
 
-			<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
-			<div class="journal-recent-block-item">
-				<div class="journal-thumbnail-wrapper">
-					<?php if ( has_post_thumbnail() ) : ?>
-						<?php the_post_thumbnail( 'large' ); ?></a>
-							<?php endif; ?>     
-								</div>
-									<div class="entry-wrapper">
-										<div class="entry-meta">
-											<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_on(); ?>
-												</div><!-- .entry-meta -->	
+							<?php endwhile; ?>
+					</div>
+	<?php else : ?>
 
-						<h3><a class="journal-title" href="<?php echo get_post_permalink() ?>"><?php the_title(); ?></a></h3>												
-							</div><!-- .entry-wrapper -->
-								<div class="entry-readmore link-button">
-									<a href="<?php echo get_post_permalink()?>">Read Entry</a>
-										</div><!-- .entry-readmore -->
-        							</div><!-- .journal-recent-block-item -->
+		<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-			<?php endforeach; wp_reset_postdata(); ?>
+	<?php endif; ?>
 
-<?php get_sidebar(); ?>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
 <?php get_footer(); ?>
--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -9,19 +9,15 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<?php while ( have_posts() ) : the_post(); ?>
-
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	        <header class="entry-header">
 		          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	        </header><!-- .entry-header -->
-
+					</header><!-- .entry-header -->
+			<div class="container">
       	<div class="entry-content">
 					<div class="about-content"><?php echo CFS () -> get('our_story'); ?></div>
-
 					<div class="about-content"><?php echo CFS () -> get('our_team'); ?></div>
-					
 	          <?php the_content(); ?>
 		        <?php
 			        wp_link_pages( array(
@@ -29,8 +25,9 @@ get_header(); ?>
 				      'after'  => '</div>',
 			) );
 		?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+					</div><!-- .entry-content -->
+			</div><!-- .container -->		
+		</article><!-- #post-## -->
         
 
 			<?php endwhile; // End of the loop. ?>
@@ -38,5 +35,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
