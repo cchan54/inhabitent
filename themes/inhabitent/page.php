9af144b8bfc8e2b94c5b46d05045fbe80d947674
Cliff Chan <cchan17@live.ca>
2018-03-06 16:00:55 -0800

    added maps and styling to content

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -2,26 +2,22 @@
 /**
  * The template for displaying all pages.
  *
- * @package RED_Starter_Theme
+ * @package Inhabitent_Starter_Theme
  */
 
 get_header(); ?>
+			<h2 class="title-text">Find Us</h2>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="container">
-				<div class="entry-content">
+			<div>
+			</div>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
-
+			<?php get_sidebar(); ?>
 		</main><!-- #main -->
-			</div> <!-- entry-content -->
-				</div> <!-- container -->
-				<?php get_sidebar(); ?>
-
 	</div><!-- #primary -->
-
 <?php get_footer(); ?>
